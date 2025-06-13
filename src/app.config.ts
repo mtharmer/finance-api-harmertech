1aade202757b6f0d41bb0f1767c9740ff474250e
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2025-06-13 12:00:14 -0400

    Add logging for environment variables

--- src/app.config.ts
+++ src/app.config.ts
@@ -6,6 +6,17 @@ import { DataSource } from "typeorm";
 import { Debt } from "./entity/Debt";
 import dotenv from 'dotenv';
 dotenv.configDotenv({path: `./.env.${process.env.NODE_ENV}`, override: true})
+import logger from "./logger";
+
+logger.info('ENVIRONMENT VARIABLES');
+logger.info('=========================================================');
+logger.info(`NODE_ENV=${process.env.NODE_ENV}`);
+logger.info(`PG_DB_URL=${process.env.PG_DB_URL}`);
+logger.info(`API_PORT=${process.env.API_PORT}`);
+logger.info(`VITE_FINANCE_API_URL=${process.env.VITE_FINANCE_API_URL}`);
+logger.info(`SUPERTOKENS_URI=${process.env.SUPERTOKENS_URI}`);
+logger.info(`SUPERTOKENS_API_KEY=${process.env.SUPERTOKENS_API_KEY}`);
+logger.info('=========================================================');
 
 export function getApiDomain() {
     const apiUrl = process.env.VITE_FINANCE_API_URL || 'http://localhost:3001';
