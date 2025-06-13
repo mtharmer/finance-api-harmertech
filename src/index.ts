64624a50a4df8d7d570e0bcbb7b7fd2fe454e5b0
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2025-06-13 16:21:24 -0400

    Remove nginx from dockerize

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import app from './app';
 import logger from './logger';
 import { db } from './app.config';
 
-const port = process.env.API_PORT || 3001
+const port = Number(process.env.API_PORT) || 3001
 
 logger.info("connecting to database...");
 try {
   if (!db.isInitialized) await db.initialize();
   logger.info('initialized database');
-  app.listen(port, () => logger.info(`API Server listening on port ${port}`));
+  app.listen(port, '127.0.0.1', 1, () => logger.info(`API Server listening on port ${port}`));
 } catch (err) {
   logger.error("failed to initialize database", err);
 }
