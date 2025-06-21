fe365cf5bca86cfab077a8147b489f7a53952afb
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2025-06-21 16:17:43 -0400

    Unit tested monthly expense serializer

---
+++ spec/serializers/monthly_expense_serializer_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MonthlyExpenseSerializer do
+  let(:monthly_expense) { build(:monthly_expense) }
+  let(:serialized) { described_class.new(monthly_expense).serializable_hash.as_json }
+  let(:attributes) { serialized.dig('data', 'attributes') }
+
+  it 'includes name' do
+    expect(attributes['name']).to eq(monthly_expense.name)
+  end
+
+  it 'includes originalBalance' do
+    expect(attributes['amount']).to eq(monthly_expense.amount.as_json)
+  end
+
+  it 'includes currentBalance' do
+    expect(attributes['expenseType']).to eq(monthly_expense.expense_type.as_json)
+  end
+
+  it 'includes apr' do
+    expect(attributes['dueDay']).to eq(monthly_expense.due_day.as_json)
+  end
+end
