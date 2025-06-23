56a0a30229fc4db160d8a70b7d165dd27593d3dd
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2025-06-23 13:04:30 -0400

    HT-72 | Created path for changing password when signed in

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -23,9 +23,18 @@ module Users
     # end
 
     # PUT /resource
-    # def update
-    #   super
-    # end
+    def update
+      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+      resource_updated = update_resource(resource, account_update_params)
+      if resource_updated
+        respond_with resource
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        respond_with resource
+      end
+    end
 
     # DELETE /resource
     # def destroy
