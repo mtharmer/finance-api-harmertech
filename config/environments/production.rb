e7f8f1abae2e77d30dd3bae2e9a3312c649bfcf0
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2025-06-25 09:45:52 -0400

    HT-79 | Configure prod to use SES mailer and set 'from' address to no-reply@harmertech.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.x.mail_from = 'no-reply@harmertech.com'
+  config.action_mailer.delivery_method = :ses
+  config.action_mailer.ses_settings = {
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region:            'us-east-1'
+  }
+  config.action_mailer.raise_delivery_errors = true
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
