cad31e0b10388c0c11286c74d184a0d3c34bb029
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2025-06-25 15:14:20 -0400

    HT-81 | Require current_balance, calculate fields in mortgage model, and improve loan calculations

--- app/models/mortgage.rb
+++ app/models/mortgage.rb
@@ -1,5 +1,33 @@
 class Mortgage < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :original_balance, :apr, :term, :payment
+  validates_presence_of :original_balance, :current_balance, :apr, :term, :payment
+
+  before_save :calculate
+
+  def calculate
+    self.original_balance_after_down_payment = original_balance - down_payment
+    calculate_remaining_term
+    calculate_original_interest
+    calculate_remaining_interest
+  end
+
+  def calculate_remaining_term
+    self.remaining_term = LoanCalculator::Payments.new(apr, current_balance, payment).result
+    self.remaining_term_after_extra = LoanCalculator::Payments.new(apr, current_balance, payment + extra_payment).result
+  end
+
+  def calculate_original_interest
+    self.original_interest = LoanCalculator::Interest.new(apr, original_balance_after_down_payment, payment,
+                                                          term).result
+    self.original_interest_after_extra = LoanCalculator::Interest.new(apr, original_balance_after_down_payment,
+                                                                      payment + extra_payment, term).result
+  end
+
+  def calculate_remaining_interest
+    self.remaining_interest = LoanCalculator::Interest.new(apr, current_balance, payment,
+                                                           remaining_term).result
+    self.remaining_interest_after_extra = LoanCalculator::Interest.new(apr, current_balance, payment + extra_payment,
+                                                                       remaining_term_after_extra).result
+  end
 end
