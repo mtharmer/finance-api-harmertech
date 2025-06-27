7ad92563962be185b8fe7416f7717a1e13b44f71
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2025-06-27 10:13:31 -0400

    Add push script

--- push.sh
+++ push.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
+set -e
 aws ecr get-login-password | docker login --username AWS --password-stdin 273058101644.dkr.ecr.us-east-1.amazonaws.com
-git pull origin main
-docker-compose pull
-docker-compose down
-sleep 1
-docker-compose up -d
-sleep 1
-yes | docker image prune
+docker-compose build
+docker-compose push
